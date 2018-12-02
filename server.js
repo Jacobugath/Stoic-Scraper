2850693c09b92fc0b7387b45d4f2d84bb23d8974
Jacob Morris <jacobthomasmorris@gmail.com>
2018-12-02 11:59:04 -0700

    Heroku Connection

--- server.js
+++ server.js
@@ -30,8 +30,10 @@ app.use(express.json());
 // Make public a static folder
 app.use(express.static("public"));
 
-// Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/unit18Populater", { useNewUrlParser: true });
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/scraper";
+
+mongoose.connect(MONGODB_URI);
 
 // Routes
 
